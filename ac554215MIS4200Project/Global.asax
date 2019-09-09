cf3f202a49631c4ff9dd9b14a563d25820dcf386
Condee <ac554215@ohio.edu>
2019-09-09 11:23:10 -0400

    Add project files.

---
+++ ac554215MIS4200Project/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ac554215MIS4200Project.MvcApplication" Language="C#" %>
---
+++ ac554215MIS4200Project/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ac554215MIS4200Project.Startup))]
+namespace ac554215MIS4200Project
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
